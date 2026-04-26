56f997d854ca9034fd6b85a5dba99816cd2e41d6
eloisaglez <eloisa.glez.medina@gmail.com>
2026-04-26 13:18:07 +0200

    Fix comments for maximum speed values in servo control

--- programas/prueba_motor_continua_pilas.ino
+++ programas/prueba_motor_continua_pilas.ino
@@ -8,14 +8,14 @@ void setup() {
   delay(2000);
 
   Serial.println("Cerrando...");
-  servo.writeMicroseconds(2000);  // maxima velocidad un sentido
+  servo.writeMicroseconds(2000);  // máxima velocidad un sentido
   delay(1500);
 
   servo.writeMicroseconds(1540);  // para
   delay(2000);
 
   Serial.println("Abriendo...");
-  servo.writeMicroseconds(1000);  // maxima velocidad otro sentido
+  servo.writeMicroseconds(1000);  // máxima velocidad otro sentido
   delay(1500);
 
   servo.writeMicroseconds(1540);  // para
